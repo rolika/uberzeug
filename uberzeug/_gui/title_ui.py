bf3df8a2440cf9b7e2f9d6a3def5f34f510f84a2
Weisz Roland <rolika@nostromo>
2024-11-10 12:21:54 +0100

    disable buttons of not implemented features

--- uberzeug/_gui/title_ui.py
+++ uberzeug/_gui/title_ui.py
@@ -2,13 +2,16 @@ import os
 from tkinter import *
 from tkinter import ttk
 
+
+TITLE = "Ü B E R Z E U G"
 TITLE_IMAGE = r"data/titleimg.gif"
-WINDOWS_ICON = "data/pohlen.ico"
-LINUX_ICON = "data/pohlen.gif"
+WINDOWS_ICON = r"data/pohlen.ico"
+LINUX_ICON = r"data/pohlen.gif"
 
 
 class TitleUI(Frame):
-    def __init__(self, root=None, title_image=TITLE_IMAGE, **kwargs) -> None:
+    def __init__(self, root:Widget=None, title:str=TITLE,
+                 title_image:str=TITLE_IMAGE, **kwargs) -> None:
         super().__init__(**kwargs)
         if os.name == "posix":
             icon = PhotoImage(file = LINUX_ICON)
@@ -16,38 +19,41 @@ class TitleUI(Frame):
         else:
             self.master.iconbitmap(default = WINDOWS_ICON)
         self.master.title("Pohlen-Dach")
+        self.__title = title
         self.__title_image = PhotoImage(file=title_image)
         self._body()
 
     def _body(self) -> None:
-        box = LabelFrame(self, text="Raktárkészlet-kezelő alkalmazás",
-                         labelanchor=N)
+        box = LabelFrame(self, text=self.__title, labelanchor=N)
         canvas = Canvas(box, width=640, height=295)
         canvas.create_image(0, 0, image=self.__title_image, anchor=NW)
         canvas.pack(padx=5, pady=5)
         box.pack(padx=5, pady=5)
         box = ttk.LabelFrame(self, text="Anyagok kezelése")
-        ttk.Button(box, text="Új anyag").pack(fill=X, padx=5, pady=5)
-        ttk.Button(box, text="Meglévő anyag módosítása")\
+        ttk.Button(box, text="Új anyag", state=DISABLED)\
             .pack(fill=X, padx=5, pady=5)
-        ttk.Button(box, text="Anyag törlése").pack(fill=X, padx=5, pady=5)
+        ttk.Button(box, text="Meglévő anyag módosítása",
+                   state=DISABLED).pack(fill=X, padx=5, pady=5)
+        ttk.Button(box, text="Anyag törlése", state=DISABLED)\
+            .pack(fill=X, padx=5, pady=5)
         box.pack(side=LEFT, fill=BOTH, padx=5, pady=5)
         box = ttk.LabelFrame(self, text="Raktárkészlet-kezelés")
         self.__withdraw_button =ttk.Button(box, text="Kivét projektre")
         self.__withdraw_button.pack(fill=X, padx=5, pady=5)
-        ttk.Button(box, text="Visszavét projektről")\
+        ttk.Button(box, text="Visszavét projektről", state=DISABLED)\
             .pack(fill=X, padx=5, pady=5)
-        ttk.Button(box, text="Új raktári tétel").pack(fill=X, padx=5, pady=5)
-        ttk.Button(box, text="Meglévő raktári tétel törlése")\
+        ttk.Button(box, text="Új raktári tétel", state=DISABLED)\
             .pack(fill=X, padx=5, pady=5)
-        ttk.Button(box, text="Raktárkészlet exportálása")\
+        ttk.Button(box, text="Meglévő raktári tétel törlése", state=DISABLED)\
+            .pack(fill=X, padx=5, pady=5)
+        ttk.Button(box, text="Raktárkészlet exportálása", state=DISABLED)\
             .pack(fill=X, padx=5, pady=5)
         box.pack(fill=BOTH, padx=5, pady=5)
-    
+
     @property
     def withdraw_button(self) -> ttk.Button:
         return self.__withdraw_button
-    
+
     @withdraw_button.setter
     def withdraw_button(self, command:callable) -> None:
         self.__withdraw_button["command"] = command
