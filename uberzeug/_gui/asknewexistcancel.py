dd60db9cefd076943f490fffacbea00edec07b4a
Weisz Roland <rolika@nostromo>
2024-12-01 11:55:50 +0100

    format ask dialog

--- uberzeug/_gui/asknewexistcancel.py
+++ uberzeug/_gui/asknewexistcancel.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from tkinter import simpledialog
 
 
@@ -6,26 +7,26 @@ class _AskNewExistCancel(simpledialog.Dialog):
     def __init__(self, parent:Widget) -> None:
         self.__answer = None
         super().__init__(parent, title="Figyelj!")
-    
+
     def body(self, parent:Widget) -> None:
-        Label(self, text="Már létezik anyag ilyen névvel és egységárral.").pack()
-    
-    def buttonbox(self) -> None:        
+        ttk.Label(self, text="Már létezik anyag ilyen névvel és egységárral.").pack(ipadx=5, ipady=5, padx=5, pady=5)
+
+    def buttonbox(self) -> None:
         box = Frame(self)
-        Button(box, text="Új tétel legyen!", command=self._newitem).pack()
-        Button(box, text="Meglévő tételhez adom.", command=self._existitem).pack()
-        Button(box, text="Mégse", command=self.cancel).pack()
+        ttk.Button(box, text="Új tétel legyen!", command=self._newitem).pack(ipadx=5, ipady=5, padx=5, pady=5, fill=X)
+        ttk.Button(box, text="Meglévő tételhez adom.", command=self._existitem).pack(ipadx=5, ipady=5, padx=5, pady=5, fill=X)
+        ttk.Button(box, text="Mégse", command=self.cancel).pack(ipadx=5, ipady=5, padx=5, pady=5, fill=X)
         self.bind("<Escape>", self.cancel)
         box.pack()
-    
+
     def _newitem(self, event:Event=None) -> None:
         self.__answer = "new"
         self.ok()
-    
+
     def _existitem(self, event:Event=None) -> None:
         self.__answer = "exist"
         self.ok()
-    
+
     @property
     def answer(self) -> str|None:
         return self.__answer
