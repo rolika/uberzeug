dadd755ef5260541591ba671ca2cdf4b82901cb4
Weisz Roland <rolika@nostromo>
2025-01-26 16:19:05 +0100

    use notebook panes

--- uberzeug/_gui/title_ui.py
+++ uberzeug/_gui/title_ui.py
@@ -39,9 +39,13 @@ class TitleUI(Frame):
             canvas.create_image(0, 0, image=self.__title_image, anchor=NW)
         canvas.pack(padx=5, pady=5)
         box.pack(padx=5, pady=5)
-        MainMenu(self).pack(fill=X, padx=5, pady=5)
-        self.__stockui = StockUI(self)
+        self.__mainmenu = ttk.Notebook(self)
+        self.__stockui = StockUI(self.__mainmenu)
         self.__stockui.pack(fill=BOTH, padx=5, pady=5)
+        self.__mainmenu.add(self.__stockui, text="Raktár")
+        self.__mainmenu.add(ttk.Frame(self.__mainmenu), text="Kontrolling")
+        self.__mainmenu.add(ttk.Frame(self.__mainmenu), text="Projektek")
+        self.__mainmenu.pack(fill=BOTH, padx=5, pady=5)
 
     @property
     def stockui(self) -> StockUI:
