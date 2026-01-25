6437e696d2bae5bb25c9d691edf043761d377a38
Weisz Roland <weisz.roland@pohlen-dach.hu>
2026-01-25 15:45:42 +0100

    add type definition

--- uberzeug/record/record.py
+++ uberzeug/record/record.py
@@ -3,7 +3,7 @@ class Record():
     translate_attributes:   a dictionary of key-value pairs, where values become
         attribute-names. If not provided, the keys of kwargs will be used.
     kwargs: key-value pairs holding the data"""
-    def __init__(self, translate_attributes=None, **kwargs) -> None:
+    def __init__(self, translate_attributes:dict=None, **kwargs) -> None:
         for arg, value in kwargs.items():
             setattr(self, translate_attributes.get(arg, arg), value)
 
