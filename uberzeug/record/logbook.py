593043d2be790dc07db4d744f5c657e7862f08c6
Weisz Roland <weisz.roland@pohlen-dach.hu>
2026-02-22 16:03:19 +0100

    remove unsused method

--- uberzeug/record/logbook.py
+++ uberzeug/record/logbook.py
@@ -10,7 +10,6 @@ class LogBook():
     def __init__(self, query:Cursor) -> None:
         self.__records = [lr.LogRecord(**item) for item in query]
         self.__records.sort(key=attrgetter("value", "change"))
-        # self._merge_same__records()
 
     @classmethod
     def from_records(cls, records:list[lr.LogRecord]) -> "LogBook":
@@ -30,18 +29,3 @@ class LogBook():
     def total(self) -> int:
         return reduce(lambda x, y: x + y,
                       (record.value for record in self.__records))
-
-    def _merge_same__records(self) -> None:
-        """Merge records with the same name and unitprice,
-        and sum their values."""
-        merged_records: list[lr.LogRecord] = []
-        for record in self.__records:
-            for merged in merged_records:
-                if (record.name == merged.name)\
-                    and (record.unitprice == merged.unitprice):
-                    merged.change += record.change
-                    merged.value += record.value
-                    break
-            else:
-                merged_records.append(record)
-        self.__records = merged_records
